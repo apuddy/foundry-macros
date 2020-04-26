1cdad368dbfe51570e86c7cdb75534da27e0ce65
Aron Puddy <aron@puddy.id.au>
2020-04-26 14:58:23 +0930

    Change two macros to use canvas.activeLayer instead of canvas.tokens.children[0] which wouldn't work if a different layer was active

--- Introduce Characters.js
+++ Introduce Characters.js
@@ -1,7 +1,5 @@
 // Change any selected tokens to always show their name, such as after the character's is introduced
 
-// Need to go through the token layer, because other ways to get the tokens dont expose _controlled?
-let allTokens = canvas.tokens.children[0].children;
-let selected = allTokens.filter(token => token._controlled == true);
+let selectedTokens = Object.values(canvas.activeLayer._controlled);
 
-selected.forEach(t => t.update({ "displayName": TOKEN_DISPLAY_MODES.ALWAYS }));
+selectedTokens.forEach(t => t.update({ "displayName": TOKEN_DISPLAY_MODES.ALWAYS }));
--- Token DisplayName Owner Hover.js
+++ Token DisplayName Owner Hover.js
@@ -1,7 +1,5 @@
 // Change any selected tokens to show names on owner hover.
 
-// Need to go through the token layer, because other ways to get the tokens dont expose _controlled?
-let allTokens = canvas.tokens.children[0].children;
-let selected = allTokens.filter(token => token._controlled == true);
+let selectedTokens = Object.values(canvas.activeLayer._controlled);
 
-selected.forEach(t => t.update({ "displayName": TOKEN_DISPLAY_MODES.OWNER_HOVER }));
+selectedTokens.forEach(t => t.update({ "displayName": TOKEN_DISPLAY_MODES.OWNER_HOVER }));
